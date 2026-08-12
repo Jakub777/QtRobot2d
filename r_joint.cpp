9520488e1604956e22114b4391e49681419cc8b0
Kuba <imejlGrucha@gmail.com>
2026-08-12 14:22:13 +0000

    add max and target speed limits for animation in joint class

--- r_joint.cpp
+++ r_joint.cpp
@@ -1,11 +1,18 @@
 #include "r_joint.h"
 
 Joint::Joint()
+    : angle(0.0)
+    , minAngle(0.0)
+    , maxAngle(0.0)
+    , speed(90.0)
+    , targetAngle(0.0)
 {
 }
 
 Joint::Joint(double a)
     : angle(a)
+    , speed(90.0)
+    , targetAngle(a)
 {
     minAngle = 10.0;
     maxAngle = 80.0;
